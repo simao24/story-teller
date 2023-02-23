1e471bcc4cbdf931d7319547f050a5e19d29a639
AnaLeon84 <ana.cableon84@gmail.com>
2023-02-23 09:21:47 +0100

    S01-DEV BRANCH- modification css file, transfer to app.css

--- src/composants/NavBarNonConnecte.svelte
+++ src/composants/NavBarNonConnecte.svelte
@@ -4,15 +4,17 @@ import logo from '../assets/logo-thestoryteller.png'
 
 <main>
     <header class="header">
-        <a href="index.html" class="header__logo">
+        <nav class="nav-bar-nonconnected">
+            <a href="index.html" class="header__logo">
             
-            <img src={logo} alt="Story Teller logo">
-        </a>
-        <form action="recherche.php" class="header__search">
-            <label for="recherche" class="header__search-label">Recherche :</label>
-            <input type="text" id="recherche" name="recherche" class="header__search-input">
-            <input type="submit" value="Rechercher" class="header__search-submit">
-        </form>
-        <a href="connexion.html" class="header__connect">Connexion</a>
+                <img src={logo} alt="Story Teller logo">
+            </a>
+            <form action="recherche.php" class="header__search">
+                <label for="recherche" class="header__search-label">Recherche :</label>
+                <input type="text" id="recherche" name="recherche" class="header__search-input">
+                <input type="submit" value="Rechercher" class="header__search-submit">
+            </form>
+            <a href="connexion.html" class="header__connect">Connexion</a>
+        </nav>
     </header>
 </main>
