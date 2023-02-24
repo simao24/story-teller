4dc4ef7f66d5467cc52b2b6be899d5598c24c6d5
mydevmarathon <mydevmarathon@gmail.com>
2023-02-24 16:51:58 +0100

    adding CreateStory

--- src/pages/createStory.svelte
+++ src/pages/createStory.svelte
@@ -1,65 +1,33 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-  
-    const dispatch = createEventDispatcher();
-    let title = '';
-    let content = '';
-    let category = '';
-    let message = '';
-  
-    function handleSubmit() {
-      const newStory = {
-        title: title,
-        content: content,
-        category: category,
-        date: new Date().toLocaleString(),
-      };
-      handleCreate(newStory); 
-    }
-  
-    function handleCreate(story) {
-      fetch('https://ylf19dmf.directus.app/items/story', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-                  "content" : content,
-                  "title": title,
-                  "category": category
-              })
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Story published:', data);
-        message = 'Votre histoire a été publiée avec succès.';
-        // Mettre à jour l'état de l'application pour refléter la nouvelle histoire publiée
-      })
-      .catch(error => {
-        console.error('Error publishing story:', error);
-        message = 'Une erreur est survenue lors de la publication de votre histoire.';
-        // Gérer les erreurs de publication de l'histoire
-      });
-    }
-  </script>
-  <div class ="container-create">
-    <h2>Partagez votre plume</h2>
-    <form on:submit|preventDefault={handleSubmit}>
-      <label for="title">Title:</label>
-      <input type="text" id="title" bind:value={title} />
-      <label for="category">Category:</label>
-      <select id="category" bind:value={category}>
-        <option value="">--Choisir une catégorie--</option>
-        <option value="Roman">Roman</option>
-        <option value="Policier">Policier</option>
-        <option value="Science-fiction">Science-fiction</option>
-        <option value="Fantasy">Fantasy</option>
-      </select>
-  
-      <textarea id="content" bind:value={content}></textarea>
-      <button type="submit">Publish</button>
-    </form>
-    {#if message}
-      <p>{message}</p>
-    {/if}
-  </div>
+  import {
+    link
+  } from "svelte-spa-router";
+</script>
+
+<main>
+  <section class="section-left">
+    <img src="vite.svg" alt="image-profil">
+    <h4>pseudo</h4>
+  </section>
+  <section class="section-centrale">
+    <h2> Profil utilisateur</h2>
+    <label for="story">ecrire une histoire</label>
+    <textarea id="story" name="story" rows="5" cols="33">
+            ça claque sa......
+        </textarea>
+
+    <button>envoyer</button>
+
+  </section>
+
+  <section class="sec-histoire">
+
+    <div class="menu-deroulant-histoires"><a href="#">menu déroulant</a>
+      <div class="sous-menu">
+        <a href="#" use:link>Modifier</a>
+        <a href="#" use:link>Supprimer</a>
+        <a href="#" use:link>ajouter une image</a>
+      </div>
+    </div>
+  </section>
+</main>
