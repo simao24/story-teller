68d2b8ca60cb0c3645d783a4f6a2b8e7c4551efe
mydevmarathon <mydevmarathon@gmail.com>
2023-02-24 16:59:16 +0100

    CreateStory

--- src/pages/CreateStory.svelte
+++ src/pages/CreateStory.svelte
@@ -1,8 +1,55 @@
 <script>
+<<<<<<< HEAD
 
   import {
     link
   } from "svelte-spa-router";
+=======
+  import {
+    link
+  } from "svelte-spa-router";
+  const dispatch = createEventDispatcher();
+  let title = '';
+  let content = '';
+  let category = '';
+  let message = '';
+
+  function handleSubmit() {
+    const newStory = {
+      title: title,
+      content: content,
+      category: category,
+      date: new Date().toLocaleString(),
+    };
+    handleCreate(newStory); 
+  }
+
+  function handleCreate(story) {
+    fetch('https://ylf19dmf.directus.app/items/story', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+                "content" : content,
+                "title": title,
+                "category": category
+            })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Story published:', data);
+      message = 'Votre histoire a été publiée avec succès.';
+      // Mettre à jour l'état de l'application pour refléter la nouvelle histoire publiée
+    })
+    .catch(error => {
+      console.error('Error publishing story:', error);
+      message = 'Une erreur est survenue lors de la publication de votre histoire.';
+      // Gérer les erreurs de publication de l'histoire
+    });
+  }
+</script>
+>>>>>>> 1a8a4f7 (CreateStory)
 </script>
 
 <main>
@@ -29,8 +76,15 @@
         <a href="#" use:link>Modifier</a>
         <a href="#" use:link>Supprimer</a>
         <a href="#" use:link>ajouter une image</a>
+<<<<<<< HEAD
 
       </div>
     </section>
   </main>
   
+=======
+      </div>
+    </div>
+  </section>
+</main>
+>>>>>>> 1a8a4f7 (CreateStory)
