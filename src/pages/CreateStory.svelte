b5d27ddf9833cdd0b0454e73b1f58b5c982bdc18
mydevmarathon <mydevmarathon@gmail.com>
2023-02-26 15:38:16 +0100

    S01-modification-CreateStory-02

--- src/pages/CreateStory.svelte
+++ src/pages/CreateStory.svelte
@@ -3,46 +3,6 @@
   import {
     link
   } from "svelte-spa-router";
-  const dispatch = createEventDispatcher();
-  let title = '';
-  let content = '';
-  let category = '';
-  let message = '';
-
-  function handleSubmit() {
-    const newStory = {
-      title: title,
-      content: content,
-      category: category,
-      date: new Date().toLocaleString(),
-    };
-    handleCreate(newStory); 
-  }
-
-  function handleCreate(story) {
-    fetch('https://ylf19dmf.directus.app/items/story', { 
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-                "content" : content,
-                "title": title,
-                "category": category
-            })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Story published:', data);
-      message = 'Votre histoire a été publiée avec succès.';
-      // Mettre à jour l'état de l'application pour refléter la nouvelle histoire publiée
-    })
-    .catch(error => {
-      console.error('Error publishing story:', error);
-      message = 'Une erreur est survenue lors de la publication de votre histoire.';
-      // Gérer les erreurs de publication de l'histoire
-    });
-  }
 </script>
 
 <main>
@@ -52,7 +12,7 @@
   </section>
   <section class="section-centrale">
     <h2> Profil utilisateur</h2>
-  
+
     <label for="story">ecrire une histoire</label>
     <textarea id="story" name="story" rows="5" cols="33">
             ça claque sa......
@@ -69,7 +29,7 @@
         <a href="#" use:link>Modifier</a>
         <a href="#" use:link>Supprimer</a>
         <a href="#" use:link>ajouter une image</a>
+
       </div>
-    </div>
-  </section>
-</main>
+    </section>
+  </main>
