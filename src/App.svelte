1042b46a39e9ca78087ef9dee5119a4f32343b70
simao24 <siminounet11@gmail.com>
2023-02-26 19:39:45 +0100

    footerandcreatestory done

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,9 @@
     import MyStory from "./pages/MyStory.svelte";
     import Favorite from './pages/Favorite.svelte';
     import StoryDetail from "./pages/StoryDetail.svelte";
+    import Footer from './composants/Footer.svelte';
+    
+    
     
     import Error from "./pages/Error.svelte";
 
@@ -36,6 +39,6 @@
     <Router {routes} />
 </main>
 <footer>
-    
+    <Footer/>
 </footer>
 
