29350f963544292b73b84471454e493e17f70739
AnaLeon84 <ana.cableon84@gmail.com>
2023-02-27 18:34:38 +0100

    S02-createstory css modification. 2 new inputs were added for title and category

--- src/pages/CreateStory.svelte
+++ src/pages/CreateStory.svelte
@@ -64,11 +64,15 @@
   </section>
   
   <section class="section-centrale">
-    <h2>Profil utilisateur</h2>
+    <h2>Mon profil</h2>
     <label for="story">Écrire une histoire</label>
+    <input type="text" id="title" name="title" placeholder="Titre" class="create__story-title">
+
+
+    <input type="text" id="category" name="category" placeholder="Categorie" class="create__story-categorie">
     <textarea id="story" name="story" rows="5" cols="33" placeholder="Entrez votre histoire ici"></textarea>
     <button>Envoyer</button>
-  </section>
+ </section>
 
   <section class="sec-histoire">
     <div class="menu-deroulant-histoires">
@@ -130,6 +134,7 @@
   }
 
   .section-centrale h2 {
+    font-family: 'Raleway', sans-serif;
     font-size: 2rem;
     font-weight: bold;
     margin-bottom: 1rem;
@@ -145,12 +150,17 @@
     padding: 0.5rem;
     border: 1px solid #ccc;
     border-radius: 0.5rem;
+    margin-top: 1rem;
     margin-bottom: 1rem;
+    margin-left: 25px;
+    margin-right: 25px;
   }
 
   .section-centrale button {
-    background-color: #007bff;
+    background-color:#1C2942;
     color: #fff;
+    font-size: large;
+    font-weight:bold ;
     padding: 0.5rem 1rem;
     border-radius: 0.5rem;
     border: none;
@@ -159,7 +169,8 @@
   }
 
   .section-centrale button:hover {
-    background-color: #0069d9;
+    background-color: #5FC2BA;
+    color:#1C2942;
   }
 
   /* Section de droite */
@@ -168,6 +179,103 @@
     flex-direction: column;
     align-items: flex
   }
+
+  #title{
+    text-align: center;
+    border:2px solid grey;
+    margin: 5px 105px;
+    border-radius: 10px;
+
+  }
+  #category{
+    text-align: center;
+    border:2px solid grey;
+    margin: 5px 105px;
+    border-radius: 10px;
+
+  }
+
+  .nav-categories ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+  
+    .nav-categories ul li {
+      display: inline-block;
+      position: relative;
+      margin-right: 20px;
+    }
+  
+    .nav-categories ul li a {
+      display: block;
+      padding: 5px 10px;
+      color: #333;
+      font-size: 16px;
+      text-decoration: none;
+    }
+  
+    .nav-categories ul li:hover > ul {
+      display: block;
+    }
+  
+    .nav-categories ul ul {
+      display: none;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      z-index: 1;
+      background-color: #f9f9f9;
+      min-width: 160px;
+      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+    }
+  
+    .nav-categories ul ul li {
+      display: block;
+      white-space: nowrap;
+    }
+  
+    .nav-categories ul ul li a {
+      font-size: 14px;
+      color: #333;
+      text-decoration: none;
+      display: block;
+      padding: 10px 20px;
+    }
+  
+    /* Animation pour le lien de catégories */
+    .nav-categories a {
+      position: relative;
+    }
+  
+    .nav-categories a::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #fff;
+      opacity: 0;
+      border-radius: 5px;
+      transition: opacity 0.9s ease;
+    }
+  
+    .nav-categories a:hover::before {
+      opacity: 1;
+      animation: flicker 1s linear infinite;
+    }
+  
+    @keyframes flicker {
+      0%,
+      100% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0.5;
+      }
+    }
+  
   </style>
 
 
